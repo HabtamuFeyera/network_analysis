2234ce7e5d631610099346802ad1ab06dbed3a81
HabtamuFeyera <habtamufeyer02@gmail.com>
2023-12-01 08:52:38 +0300

    modified the changes:   src/config.py

--- src/config.py
+++ src/config.py
@@ -1,8 +1,14 @@
 
-# config.py
+from __future__ import print_function
+import argparse
 
-SLACK_DATA_PATH = "/home/habte/Anonymized_B6SlackExport_25Nov23"
-API_KEY = "your_api_key"
-# Add other configurations as needed
+parser = argparse.ArgumentParser(description='cmdArgs')
+parser.add_argument('--output', type=str, default='slack_data.csv',
+                help='filename to write analysis output in CSV format')
+parser.add_argument('--path', required=True, type=str, help='directory where slack data reside')
+parser.add_argument('--channel', type=str, default='', help='which channel we parsing')
+parser.add_argument('--userfile', type=str, default='users.json', help='users profile information')
 
+cfg = parser.parse_args()
+# print(cfg)
 
