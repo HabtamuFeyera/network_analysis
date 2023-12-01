660971c8828874cf847993f3d856d033f0db4a13
HabtamuFeyera <habtamufeyer02@gmail.com>
2023-12-01 09:09:25 +0300

    modified the given script:   src/utils.py

--- src/utils.py
+++ src/utils.py
@@ -1,20 +1,174 @@
-import json
+import os
+import sys
 import glob
-import pandas as pd
+import json
 import datetime
-import re
+import pandas as pd
+from matplotlib import pyplot as plt
+import seaborn as sns
+from nltk.corpus import stopwords
+from collections import Counter
+from collections import Counter
 
-def combine_json_files(file_paths):
-    """Combine data from multiple JSON files."""
-    combined_data = []
-    for file_path in file_paths:
-        with open(file_path, 'r', encoding="utf8") as slack_data:
-            combined_data.extend(json.load(slack_data))
-    return combined_data
+
+def break_combined_weeks(combined_weeks):
+    """
+    Breaks combined weeks into separate weeks.
+    
+    Args:
+        combined_weeks: list of tuples of weeks to combine
+        
+    Returns:
+        tuple of lists of weeks to be treated as plus one and minus one
+    """
+    plus_one_week = []
+    minus_one_week = []
+
+    for week in combined_weeks:
+        if week[0] < week[1]:
+            plus_one_week.append(week[0])
+            minus_one_week.append(week[1])
+        else:
+            minus_one_week.append(week[0])
+            plus_one_week.append(week[1])
+
+    return plus_one_week, minus_one_week
+
+def get_msgs_df_info(df):
+    msgs_count_dict = df.user.value_counts().to_dict()
+    replies_count_dict = dict(Counter([u for r in df.replies if r != None for u in r]))
+    mentions_count_dict = dict(Counter([u for m in df.mentions if m != None for u in m]))
+    links_count_dict = df.groupby("user").link_count.sum().to_dict()
+    return msgs_count_dict, replies_count_dict, mentions_count_dict, links_count_dict
+
+
+
+def get_messages_dict(msgs):
+    msg_list = {
+            "msg_id":[],
+            "text":[],
+            "attachments":[],
+            "user":[],
+            "mentions":[],
+            "emojis":[],
+            "reactions":[],
+            "replies":[],
+            "replies_to":[],
+            "ts":[],
+            "links":[],
+            "link_count":[]
+            }
+
+
+    for msg in msgs:
+        if "subtype" not in msg:
+            try:
+                msg_list["msg_id"].append(msg["client_msg_id"])
+            except:
+                msg_list["msg_id"].append(None)
+            
+            msg_list["text"].append(msg["text"])
+            msg_list["user"].append(msg["user"])
+            msg_list["ts"].append(msg["ts"])
+            
+            if "reactions" in msg:
+                msg_list["reactions"].append(msg["reactions"])
+            else:
+                msg_list["reactions"].append(None)
+
+            if "parent_user_id" in msg:
+                msg_list["replies_to"].append(msg["ts"])
+            else:
+                msg_list["replies_to"].append(None)
+
+            if "thread_ts" in msg and "reply_users" in msg:
+                msg_list["replies"].append(msg["replies"])
+            else:
+                msg_list["replies"].append(None)
+            
+            if "blocks" in msg:
+                emoji_list = []
+                mention_list = []
+                link_count = 0
+                links = []
+                
+                for blk in msg["blocks"]:
+                    if "elements" in blk:
+                        for elm in blk["elements"]:
+                            if "elements" in elm:
+                                for elm_ in elm["elements"]:
+                                    
+                                    if "type" in elm_:
+                                        if elm_["type"] == "emoji":
+                                            emoji_list.append(elm_["name"])
+
+                                        if elm_["type"] == "user":
+                                            mention_list.append(elm_["user_id"])
+                                        
+                                        if elm_["type"] == "link":
+                                            link_count += 1
+                                            links.append(elm_["url"])
+
+
+                msg_list["emojis"].append(emoji_list)
+                msg_list["mentions"].append(mention_list)
+                msg_list["links"].append(links)
+                msg_list["link_count"].append(link_count)
+            else:
+                msg_list["emojis"].append(None)
+                msg_list["mentions"].append(None)
+                msg_list["links"].append(None)
+                msg_list["link_count"].append(0)
+    
+    return msg_list
+
+def from_msg_get_replies(msg):
+    replies = []
+    if "thread_ts" in msg and "replies" in msg:
+        try:
+            for reply in msg["replies"]:
+                reply["thread_ts"] = msg["thread_ts"]
+                reply["message_id"] = msg["client_msg_id"]
+                replies.append(reply)
+        except:
+            pass
+    return replies
+
+def msgs_to_df(msgs):
+    msg_list = get_messages_dict(msgs)
+    df = pd.DataFrame(msg_list)
+    return df
+
+def process_msgs(msg):
+    '''
+    select important columns from the message
+    '''
+
+    keys = ["client_msg_id", "type", "text", "user", "ts", "team", 
+            "thread_ts", "reply_count", "reply_users_count"]
+    msg_list = {k:msg[k] for k in keys}
+    rply_list = from_msg_get_replies(msg)
+
+    return msg_list, rply_list
+
+def get_messages_from_channel(channel_path):
+    '''
+    get all the messages from a channel        
+    '''
+    channel_json_files = os.listdir(channel_path)
+    channel_msgs = [json.load(open(channel_path + "/" + f)) for f in channel_json_files]
+
+    df = pd.concat([pd.DataFrame(get_messages_dict(msgs)) for msgs in channel_msgs])
+    print(f"Number of messages in channel: {len(df)}")
+    
+    return df
 
 
 def convert_2_timestamp(column, data):
-    """Convert from unix time to readable timestamp."""
+    """convert from unix time to readable timestamp
+        args: column: columns that needs to be converted to timestamp
+                data: data that has the specified column
+    """
     if column in data.columns.values:
         timestamp_ = []
         for time_unix in data[column]:
@@ -24,28 +178,4 @@ def convert_2_timestamp(column, data):
                 a = datetime.datetime.fromtimestamp(float(time_unix))
                 timestamp_.append(a.strftime('%Y-%m-%d %H:%M:%S'))
         return timestamp_
-    else:
-        print(f"{column} not in data")
-
-
-def get_tagged_users(df):
-    """Get all @ in the messages."""
-    return df['msg_content'].map(lambda x: re.findall(r'@U\w+', x))
-
-
-def get_community_participation(path):
-    """Get community participation."""
-    combined = []
-    comm_dict = {}
-    for json_file in glob.glob(f"{path}*.json"):
-        with open(json_file, 'r') as slack_data:
-            combined.append(slack_data)
-
-    for i in combined:
-        a = json.load(open(i.name, 'r', encoding='utf-8'))
-        for msg in a:
-            if 'replies' in msg.keys():
-                for i in msg['replies']:
-                    comm_dict[i['user']] = comm_dict.get(i['user'], 0) + 1
-    return comm_dict
-
+    else: print(f"{column} not in data")
