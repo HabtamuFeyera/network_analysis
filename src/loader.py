577b7ba29d1be6735c249b09ad635a727b97cd91
HabtamuFeyera <habtamufeyer02@gmail.com>
2023-12-03 14:57:17 +0300

    modified:   src/loader.py

--- src/loader.py
+++ src/loader.py
@@ -29,40 +29,38 @@ class SlackDataLoader:
             user_ids_by_name[user['name']] = user['id']
         return user_names_by_id, user_ids_by_name
 
-    def get_tagged_users(df):
+    def get_tagged_users(self, df):
         return df['msg_content'].map(lambda x: re.findall(r'@U\w+', x))
 
-    def get_community_participation(path):
+    def get_community_participation(self):
         combined = []
         comm_dict = {}
-        for json_file in glob.glob(f"{path}*.json"):
+        for json_file in glob.glob(os.path.join(self.path, '*.json')):
             with open(json_file, 'r') as slack_data:
-                combined.append(slack_data)
+                combined.append(json.load(slack_data))
         for i in combined:
-            a = json.load(i)
-            if not isinstance(a, list):
-                print(f"Invalid JSON format in file: {i.name}")
+            if not isinstance(i, list):
+                print(f"Invalid JSON format in file: {i}")
                 continue
-            for msg in a:
+            for msg in i:
                 if 'replies' in msg.keys():
-                    for i in msg['replies']:
-                        comm_dict[i['user']] = comm_dict.get(i['user'], 0) + 1
+                    for reply in msg['replies']:
+                        comm_dict[reply['user']] = comm_dict.get(reply['user'], 0) + 1
         return comm_dict
 
-    def get_messages_from_channel(channel_path):
+    def get_messages_from_channel(self, channel_path):
         channel_json_files = os.listdir(channel_path)
         channel_msgs = [json.load(open(os.path.join(channel_path, f), 'r', encoding='utf-8')) for f in channel_json_files]
 
-        df = pd.concat([pd.DataFrame(get_messages_dict(msgs)) for msgs in channel_msgs])
+        df = pd.concat([pd.DataFrame(self.get_messages_dict(msgs)) for msgs in channel_msgs])
         print(f"Number of messages in channel: {len(df)}")
 
         return df
 
-    def get_messages_dict(msgs):
-        # You should implement this function based on your specific data structure.
+    def get_messages_dict(self, msgs):
+        # Placeholder: Implement this function based on your specific data structure.
         # It's not clear from the provided code how the messages are structured.
         # You might need to adjust this function accordingly.
-
         # Example: Return a dictionary with keys like 'msg_type', 'msg_content', etc.
         pass
 
@@ -70,3 +68,13 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Export Slack history')
     parser.add_argument('--zip', help="Name of a zip file to import")
     args = parser.parse_args()
+
+    if args.zip:
+        slack_loader = SlackDataLoader(args.zip)
+        # Call the necessary methods to load and process the Slack data
+        user_map = slack_loader.get_user_map()
+        tagged_users = slack_loader.get_tagged_users(df)  # Replace df with your DataFrame
+        community_participation = slack_loader.get_community_participation()
+        # Add more method calls as needed
+    else:
+        print("Please provide a zip file.")
