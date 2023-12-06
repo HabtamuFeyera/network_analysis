f3ad85bcaf7e0a0b33d95d12abb392aba1b088ff
HabtamuFeyera <habtamufeyer02@gmail.com>
2023-12-06 15:07:53 +0300

    modified:   src/plotter.py

--- src/plotter.py
+++ src/plotter.py
@@ -1,70 +1,31 @@
 import os
-import sys
-import glob
-import json
-import datetime
 import nltk
-nltk.download('stopwords')
-nltk.download('stopwords')
-from collections import Counter
 import pandas as pd
 import matplotlib.pyplot as plt
 from nltk.corpus import stopwords
 from wordcloud import WordCloud
 
+# Download NLTK stopwords
+nltk.download('stopwords')
 
-def get_top_10_user(data, channel='Random'):
-    """get user with the highest number of message sent to any channel"""
 
-    data['sender_name'].value_counts()[:10].plot.bar(figsize=(10, 3))
-    plt.title(f'Top 10 Message Senders in #{channel} channels', size=15, fontweight='bold')
-    plt.xlabel("Sender Name", size=18); plt.ylabel("Frequency", size=14);
-    plt.xticks(size=12); plt.yticks(size=12);
-    plt.show()
+def get_top_10_user(df, channel='Random'):
+    """Get the top 10 users with the highest number of messages sent to any channel."""
 
-    data['sender_name'].value_counts()[-10:].plot.bar(figsize=(15, 7.5))
-    plt.title(f'Bottom 10 Message Senders in #{channel} channels', size=15, fontweight='bold')
-    plt.xlabel("Sender Name", size=18); plt.ylabel("Frequency", size=14);
-    plt.xticks(size=12); plt.yticks(size=12);
-    plt.show()
+def draw_avg_reply_count(df, channel='Random'):
+    """Visualize the average number of reply counts per sender."""
+    
 
-def draw_avg_reply_count(data, channel='Random'):
-    """who commands many reply?"""
-
-    data.groupby('sender_name')['reply_count'].mean().sort_values(ascending=False)[:20]\
-        .plot(kind='bar', figsize=(10, 3));
-    plt.title(f'Average Number of reply count per Sender in #{channel}', size=20, fontweight='bold')
-    plt.xlabel("Sender Name", size=18); plt.ylabel("Frequency", size=18);
-    plt.xticks(size=14); plt.yticks(size=14);
-    plt.show()
-
-def draw_avg_reply_users_count(data, channel='Random'):
-    """who commands many user reply?"""
-
-    data.groupby('sender_name')['reply_users_count'].mean().sort_values(ascending=False)[:20].plot(kind='bar',
-     figsize=(10,3));
-    plt.title(f'Average Number of reply user count per Sender in #{channel}', size=20, fontweight='bold')
-    plt.xlabel("Sender Name", size=18); plt.ylabel("Frequency", size=18);
-    plt.xticks(size=14); plt.yticks(size=14);
-    plt.show()
+def draw_avg_reply_users_count(df, channel='Random'):
+    """Visualize the average number of reply user counts per sender."""
+    
 
 def draw_wordcloud(msg_content, week):    
-    # word cloud visualization
-    allWords = ' '.join([twts for twts in msg_content])
-    wordCloud = WordCloud(background_color='#975429', width=500, height=300, random_state=21, max_words=500, mode='RGBA',
-                            max_font_size=140, stopwords=stopwords.words('english')).generate(allWords)
-    plt.figure(figsize=(15, 7.5))
-    plt.imshow(wordCloud, interpolation="bilinear")
-    plt.axis('off')
-    plt.tight_layout()
-    plt.title(f'WordCloud for {week}', size=30)
-    plt.show()
+    """Generate and display a WordCloud for the given message content."""
+    
+def draw_user_reaction(df, channel='General'):
+    """Visualize users with the most reactions in the specified channel."""
+    
 
-def draw_user_reaction(data, channel='General'):
-    data.groupby('sender_name')[['reply_count', 'reply_users_count']].sum()\
-        .sort_values(by='reply_count',ascending=False)[:10].plot(kind='bar', figsize=(10, 3))
-    plt.title(f'User with the most reaction in #{channel}', size=25);
-    plt.xlabel("Sender Name", size=18); plt.ylabel("Frequency", size=18);
-    plt.xticks(size=14); plt.yticks(size=14);
-    plt.show()
 
+plt.show()  
